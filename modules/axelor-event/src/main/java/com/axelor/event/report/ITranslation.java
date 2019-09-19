6841f047b6ba59b88e4a0f0f05b0473a4f0c3c86
naveenkumarswami <nsw.axelor@gmail.com>
2019-09-19 10:35:05 +0530

    add the translation

---
+++ modules/axelor-event/src/main/java/com/axelor/event/report/ITranslation.java
@@ -0,0 +1,20 @@
+
+package com.axelor.event.report;
+
+public interface ITranslation {
+
+  public static final String REFERENCE = /*$$(*/ "Event.reference"; /*)*/
+  public static final String START_DATE = /*$$(*/ "Event.startDateT"; /*)*/
+  public static final String END_DATE = /*$$(*/ "Event.endDateT"; /*)*/
+  public static final String REGISTRATION_OPEN = /*$$(*/ "Event.registrationOpenDate"; /*)*/
+  public static final String REGISTRATION_CLOSE = /*$$(*/ "Event.registrationCloseDate"; /*)*/
+  public static final String CAPACITY = /*$$(*/ "Event.capacity"; /*)*/
+  public static final String EVENT_FEES = /*$$(*/ "Event.eventFees"; /*)*/
+  public static final String TOTAL_ENTRY = /*$$(*/ "Event.totalEntry"; /*)*/
+  public static final String AMOUNT_COLLECTED = /*$$(*/ "Event.amountCollected"; /*)*/
+  public static final String TOTAL_DISCOUNT = /*$$(*/ "Event.totalDiscount"; /*)*/
+  public static final String EMAIL = /*$$(*/ "EventRegistration.email"; /*)*/
+  public static final String REGISTRATION_DATE = /*$$(*/ "EventRegistration.registrationDateT"; /*)*/
+  public static final String EVENT_REGISTRATION_AMOUNT = /*$$(*/ "EventRegistration.amount"; /*)*/
+
+}
